e9b610d5b7e15e855436d7a4aae8e635eee9d11e
David-Peralta-Rd <crdavidperalta@gmail.com>
2026-07-31 03:13:27 -0500

    Agregamos reglas(src/.config/hypr/windowrules.lua): Se agregaron las reglas para poner flotantes las imagenes y reproductores de video.

--- src/.config/hypr/hyprland/windowrules.lua
+++ src/.config/hypr/hyprland/windowrules.lua
@@ -9,3 +9,11 @@ Rules.forClass("hyprland-run"):custom({
     move  = "20 monitor_h-120",
     float = true,
 })
+
+
+-- Rules para mvp y imv(imagenes y videos)
+local imv = Rules.forClass("imv")
+imv:custom({ float = true, size = "70% 70%", center = true })
+
+local mpv = Rules.forClass("mpv")
+mpv:custom({ float = true, size = "60% 60%", center = true })
