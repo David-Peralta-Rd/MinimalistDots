0bad4e8d64be6acbff2b8985cac6a9cbceb7c3a3
David-Peralta-Rd <crdavidperalta@gmail.com>
2026-08-30 16:07:52 -0500

    Arrelgo("src/config/hypr/hyprland/env.lua"): Solucion de variables: las variables estaban siendo llamadas de la manera antigua, lo cual esto generaba un error al ejecutarse. Se adaptaron al nuevo modelo de hyprland que usa el lenguaje de lua.

--- src/config/hypr/hyprland/env.lua
+++ src/config/hypr/hyprland/env.lua
@@ -3,10 +3,10 @@
 -- VE A LA SIGUIENTE CARPETA Y REALIZA TUS CONFIGURACIONES PERSONALES:              --
 -- ~/.config/hypr/custom_minimalist/env.lua                                         --
 --------------------------------------------------------------------------------------
-# Forzar el tema moderno de Hyprland
-env = HYPRCURSOR_THEME,Bibata-Modern-Classic
-env = HYPRCURSOR_SIZE,20
+-- Forzar el tema moderno de Hyprland
+hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("HYPRCURSOR_SIZE", "20")
 
-# Fallback para aplicaciones XWayland / Legadas
-env = XCURSOR_THEME,Bibata-Modern-Classic
-env = XCURSOR_SIZE,20
+-- Fallback para aplicaciones XWayland / Legadas
+hl.env("XCURSOR_THEME", "Bibata-Modern-Classic")
+hl.env("XCURSOR_SIZE", "20")
