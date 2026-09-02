45d83d469643901697a78ca770d474f7793636d4
David-Peralta-Rd <crdavidperalta@gmail.com>
2026-09-02 07:37:07 -0500

    Actualizacion("install.sh"): Se cargo el lenguaje.

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ SRC_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 LANG_DIR="$SRC_DIR/src/lang"
 
 # Carga la configuración de idioma
-source "$SRC_DIR/load_lang.sh"
+source "$LANG_DIR/load_lang.sh"
 
 # Cargando instalacion por pasos:
 bash "$SRC_DIR/src/scripts/steps.sh"
