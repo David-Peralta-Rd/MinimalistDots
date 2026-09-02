c1753eb2aa07828afbf6b26d88e9535a7f374bd4
David-Peralta-Rd <crdavidperalta@gmail.com>
2026-09-02 18:53:08 -0500

    Actualizando("src/.config/hypr/hyprland/keybinds.lua"): Agregando complejidad de comando a una variable guardada.

--- src/.config/hypr/hyprland/keybinds.lua
+++ src/.config/hypr/hyprland/keybinds.lua
@@ -52,8 +52,8 @@ kb:category("Grabación")
 -- MULTIMEDIA
 -- ---------------------------------------------------------
 kb:category("Multimedia")
-  :exec("SHIFT+I", vars.volume_up, "Subir volumen / Turn up the volume")
-  :exec("SHIFT+K", vars.volume_down, "Bajar volumen / Lower volume")
+--  :exec("SHIFT+I", vars.volume_up, "Subir volumen / Turn up the volume")
+--  :exec("SHIFT+K", vars.volume_down, "Bajar volumen / Lower volume")
 
 Keybinder.media("XF86AudioRaiseVolume", "wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%+", "Subir volumen / Turn up the volume", true)
 Keybinder.media("XF86AudioLowerVolume", "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%-",       "Bajar volumen / Lower volume", true)
