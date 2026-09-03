f57f7ce25d472811375593bf37599231fc28ef6f
David-Peralta-Rd <crdavidperalta@gmail.com>
2026-09-02 19:36:34 -0500

    Actualizando("src/.config/hypr/hyprland/vars.lua"): Usamos variables para la subida, bajada y muteo de volumen.

--- src/.config/hypr/hyprland/vars.lua
+++ src/.config/hypr/hyprland/vars.lua
@@ -21,16 +21,15 @@ return {
 -- =========================== --
     cliphist = "cliphist list | wofi -c " .. HOME .. "/.config/wofi/configs/config-clipboard -s " .. HOME .. "/.config/wofi/themes/style-clipboard.css --dmenu | cliphist decode | wl-copy",
 
-    volume_up = [[bash -c "wpctl set-volume -l 1.0 @DEFAULT_AUDIO_SINK@ 5%+ && VOL=\$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int(\$2 * 100)}') && notify-send -e -u low -h string:x-canonical-private-synchronous:volume 'Volumen' \"\${VOL}%\""]],
-
-    volume_down = [[bash -c "wpctl set-volume @DEFAULT_AUDIO_SINK@ 5%- && VOL=\$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | awk '{print int(\$2 * 100)}') && notify-send -e -u low -h string:x-canonical-private-synchronous:volume 'Volumen' \"\${VOL}%\""]],
-
 -- ========================== --
 -- ==== Keybinds Scripts ==== --
 -- ========================== --
-    screenshot      = HOME .. "/.local/bin/MinimalistDots/scripts/screenshot.sh",
-    select_wallpaper = HOME .. "/.local/bin/MinimalistDots/scripts/select_wallpaper.sh",
-    show_binds      = HOME .. "/.local/bin/MinimalistDots/scripts/show_binds",
-    screenrecord    = HOME .. "/.local/bin/MinimalistDots/scripts/screenrecord.sh",
-    process_manager = HOME .. "/.local/bin/MinimalistDots/scripts/process_manager.sh"
+    volume_up           = HOME .. "/.local/bin/MinimalistDots/scripts/volume.sh up",
+    volume_down         = HOME .. "/.local/bin/MinimalistDots/scripts/volume.sh down",
+    volume_mute         = HOME .. "/.local/bin/MinimalistDots/scripts/volume.sh mute",
+    screenshot          = HOME .. "/.local/bin/MinimalistDots/scripts/screenshot.sh",
+    select_wallpaper    = HOME .. "/.local/bin/MinimalistDots/scripts/select_wallpaper.sh",
+    show_binds          = HOME .. "/.local/bin/MinimalistDots/scripts/show_binds",
+    screenrecord        = HOME .. "/.local/bin/MinimalistDots/scripts/screenrecord.sh",
+    process_manager     = HOME .. "/.local/bin/MinimalistDots/scripts/process_manager.sh"
 }
